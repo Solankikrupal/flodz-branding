f8b7a46bce413afb86ca637c05458aba1c718bfe
krupalglasier <krupal@glasier.in>
2023-08-23 19:10:41 +0530

    site developed

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import ChooseUs from "./components/ChooseUs";
 import Process from "./components/Process";
 import Services from "./components/Services";
 import CaseStudy from "./components/CaseStudy";
-/* import ContactUs from "./components/ContactUs";
-import Footer from "./components/Footer"; */
+import ContactUs from "./components/ContactUs";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,8 +15,8 @@ function App() {
       <Process />
       <Services />
       <CaseStudy />
-      {/*  <ContactUs />
-      <Footer /> */}
+      <ContactUs />
+      <Footer />
     </div>
   );
 }
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,134 +2,74 @@ import React from "react";
 
 function ContactUs() {
   return (
-    <section class="relative flex items-top justify-center min-h-screen  dark:bg-gray-900 sm:items-center sm:pt-0">
-      <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-        <div class="mt-8 overflow-hidden">
-          <div class="grid grid-cols-1 md:grid-cols-2">
-            <div class="p-6 mr-2 bg-gray-100 dark:bg-gray-800 sm:rounded-lg">
-              <h1 class="text-4xl sm:text-5xl text-gray-800 dark:text-white font-extrabold tracking-tight">
-                Get in touch
-              </h1>
-              <p class="text-normal text-lg sm:text-2xl font-medium text-gray-600 dark:text-gray-400 mt-2">
-                Fill in the form to start a conversation
-              </p>
+    <section class="py-20 px-10 bg-contact-us-section">
+      <div class="grid grid-cols-12">
+        <div class="col-span-6 ">
+          <h1 className="text-4xl font-bold tracking-tight leading-none md:text-4xl xl:text-4x text-wheat-white mb-1">
+            How Can We <p> Help?</p>
+          </h1>
+          <p class=" font-normal text-md text-white mt-2">
+            Our Experts will reach you with in 48hrs.
+          </p>
+        </div>
 
-              <div class="flex items-center mt-8 text-gray-600 dark:text-gray-400">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="1.5"
-                  viewBox="0 0 24 24"
-                  class="w-8 h-8 text-gray-500"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1.5"
-                    d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+        <div class="col-span-6  items-center justify-start">
+          <div class="mx-auto w-full max-w-lg">
+            <form action="https://api.web3forms.com/submit">
+              <input
+                type="hidden"
+                name="access_key"
+                value="YOUR_ACCESS_KEY_HERE"
+              />
+              <div class="grid gap-6 sm:grid-cols-2">
+                <div class="relative z-0">
+                  <input
+                    type="text"
+                    name="name"
+                    class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                    placeholder=" "
                   />
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1.5"
-                    d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                  <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-md font-normal text-white opacity-30  duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                    Name
+                  </label>
+                </div>
+                <div class="relative z-0">
+                  <input
+                    type="text"
+                    name="email"
+                    class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                    placeholder=" "
                   />
-                </svg>
-                <div class="ml-4 text-md tracking-wide font-semibold w-40">
-                  Acme Inc, Street, State, Postal Code
+                  <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-md font-normal text-white opacity-30  duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                    Phone Number
+                  </label>
+                </div>
+                <div class="relative z-0 col-span-2">
+                  <input
+                    type="text"
+                    name="name"
+                    class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                    placeholder=" "
+                  />
+                  <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-md font-normal text-white opacity-30  duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                    Email
+                  </label>
+                </div>
+                <div class="relative z-0 col-span-2">
+                  <textarea
+                    name="message"
+                    rows="3"
+                    class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                    placeholder=" "
+                  ></textarea>
+                  <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-md font-normal text-white opacity-30 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                    Notes (optional)
+                  </label>
                 </div>
               </div>
-
-              <div class="flex items-center mt-4 text-gray-600 dark:text-gray-400">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="1.5"
-                  viewBox="0 0 24 24"
-                  class="w-8 h-8 text-gray-500"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1.5"
-                    d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                  />
-                </svg>
-                <div class="ml-4 text-md tracking-wide font-semibold w-40">
-                  +44 1234567890
-                </div>
-              </div>
-
-              <div class="flex items-center mt-2 text-gray-600 dark:text-gray-400">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="1.5"
-                  viewBox="0 0 24 24"
-                  class="w-8 h-8 text-gray-500"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1.5"
-                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                  />
-                </svg>
-                <div class="ml-4 text-md tracking-wide font-semibold w-40">
-                  info@acme.org
-                </div>
-              </div>
-            </div>
-
-            <form class="p-6 flex flex-col justify-center">
-              <div class="flex flex-col">
-                <label for="name" class="hidden">
-                  Full Name
-                </label>
-                <input
-                  type="name"
-                  name="name"
-                  id="name"
-                  placeholder="Full Name"
-                  class="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
-                />
-              </div>
-
-              <div class="flex flex-col mt-2">
-                <label for="email" class="hidden">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Email"
-                  class="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
-                />
-              </div>
-
-              <div class="flex flex-col mt-2">
-                <label for="tel" class="hidden">
-                  Number
-                </label>
-                <input
-                  type="tel"
-                  name="tel"
-                  id="tel"
-                  placeholder="Telephone Number"
-                  class="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
-                />
-              </div>
-
               <button
                 type="submit"
-                class="md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300"
+                class="mt-5 rounded-md border border-white px-10 py-2 text-white"
               >
                 Submit
               </button>
