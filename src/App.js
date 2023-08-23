5118dc60575609f611c0ed45b197780b00a01fa5
krupalglasier <krupal@glasier.in>
2023-08-23 17:03:17 +0530

    resolve

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import ChooseUs from "./components/ChooseUs";
 import Process from "./components/Process";
 import Services from "./components/Services";
 import CaseStudy from "./components/CaseStudy";
-import ContactUs from "./components/ContactUs";
-import Footer from "./components/Footer";
+/* import ContactUs from "./components/ContactUs";
+import Footer from "./components/Footer"; */
 
 function App() {
   return (
