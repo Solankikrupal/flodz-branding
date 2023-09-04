9e7134e1adce2e5ba502a560d944904f5e28cd1b
krupalglasier <krupal@glasier.in>
2023-09-04 11:50:13 +0530

    rm unused

--- src/components/home/CaseStudy.js
+++ src/components/home/CaseStudy.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
+/* import { Carousel } from "react-responsive-carousel"; */
 
 function CaseStudy() {
   let data = [
