4d2a6627fa6005be36385177e4c9787e1819ebe3
Solankikrupal <krplsolanki@gmail.com>
2023-09-06 00:46:36 +0530

    feedback

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -43,14 +43,18 @@ function NavBar() {
           >
             CaseStudy
           </Link>
-          <Direct
-            to="/contact-us"
+          <Link
+            to="feedback"
+            spy={true}
+            smooth={true}
+            offset={-70} // Adjust offset as needed
+            duration={600}
             className="flex items-center justify-center  text-base text-wheat-white  font-normal tracking-tight transition duration-150 ease-in-out p-2 cursor-pointer"
           >
             <button className="px-6 py-3 rounded-lg font-normal border text-sm  text-white border-gra border-gradient-to-r from-btn-purple to-btn-secondary nav-hover-primary-btn">
               Contact Us
             </button>
-          </Direct>
+          </Link>
         </div>
       </>
     </div>
